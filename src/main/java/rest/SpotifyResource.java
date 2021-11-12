9e5e790131a8a49a1fdc5bd982f1cb2e17ac923b
Alek0329 <alek0329@gmail.com>
2021-11-12 16:31:48 +0100

    added endpoint spotify/token

--- src/main/java/rest/SpotifyResource.java
+++ src/main/java/rest/SpotifyResource.java
@@ -1,4 +1,28 @@
 package rest;
 
+import facades.FacadeExample;
+import facades.SpotifyFacade;
+import utils.EMF_Creator;
+
+import javax.persistence.EntityManagerFactory;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
+@Path("spotify")
 public class SpotifyResource {
+
+    @GET
+    @Produces
+    @Path("token")
+    public String getTokenFromSpotify() throws IOException {
+        EntityManagerFactory emf = EMF_Creator.createEntityManagerFactory();
+        SpotifyFacade sf = SpotifyFacade.getSpotifyFacade(emf);
+
+        String result = SpotifyFacade.getTokenFromSpotify();
+
+        return result;
+    }
 }
